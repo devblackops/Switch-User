b15eb7609920025d3af407bd333c2507fe944643
Brandon Olin <brandon@devblackops.io>
2015-11-10 22:48:55 -0800

    Update formating

--- Switch-User.psm1
+++ Switch-User.psm1
@@ -1,62 +1,56 @@
 ﻿#starts a new powershell console with specified credentials, similar to su(1) on UNIX
-function Switch-User {
-    param(
-      [string] $Username  
-    )
-    
-	if( -not $Username ) {
+
+function Switch-User( [String] $username ) {
+    if( !$username ) {
 		#look up the built-in Administrator account using WMI.
 		#the built-in administrator has a SID that starts with S-1-5 and ends with -500.
-		$accts = Get-WmiObject -Class win32_useraccount
+
+		$accts = get-wmiobject win32_useraccount
 
 		foreach( $acct in $accts ) {
 			if( $acct.SID -match '^S-1-5-.+-500$' ) {
-				$Username = $acct.Caption
-				if( $Username -match "[^\\]+$" ) {
-					$Username = $matches[0]
+				$username = $acct.Caption
+				if( $username -match "[^\\]+$" ) {
+					$username = $matches[0]
 				}
 				break
 			}
 		}
 	}
 
-	$credential = Get-ConsoleCredential -Username $Username
-	$startInfo = New-Object -TypeName Diagnostics.ProcessStartInfo
-	$startInfo.UseShellExecute = $false
-	$startInfo.FileName = "$pshome\powershell.exe"
-	$startInfo.UserName = $credential.UserName
-	$startInfo.Password = $credential.Password
-	$startInfo.WorkingDirectory = $pwd
+	$credential = Get-ConsoleCredential( $username )
+	$startinfo = new-object Diagnostics.ProcessStartInfo
+	$startinfo.UseShellExecute = $false
+	$startinfo.FileName = "$pshome\powershell.exe"
+	$startinfo.UserName = $credential.UserName
+	$startinfo.Password = $credential.Password
+	$startinfo.WorkingDirectory = $pwd
 
 	trap [ComponentModel.Win32Exception] {
 		if( $_.Exception.NativeErrorCode -eq 267 ) {
-			Write-Host -Object "$pwd is an invalid directory for $Username."
-			Write-Host -Object "Starting PowerShell in ${env:SystemRoot}\system32."
-			$startInfo.WorkingDirectory = "${env:SystemRoot}\system32"
-			$null = [Diagnostics.Process]::Start( $startInfo )
+			write-host "$pwd is an invalid directory for $username."
+			write-host "Starting PowerShell in ${env:SystemRoot}\system32."
+			$startinfo.WorkingDirectory = "${env:SystemRoot}\system32"
+			$null = [Diagnostics.Process]::Start( $startinfo )
 		} else {
 			$_.Exception.Message
 			$_.Exception.NativeErrorCode
 		}
+
 		continue
 	}
-	$null = [Diagnostics.Process]::Start( $startInfo )
+	$null = [Diagnostics.Process]::Start( $startinfo )
 }
 
 #Generate a PSCredential object without creating a pop-up security dialog like
 #the built-in get-credential cmdlet.
-function Get-ConsoleCredential {
-    param(
-        [string] $Username = ( Read-Host -Prompt 'Username' ) 
-    )
-    
-	while ( -not $Username ) {
-		$Username = Read-Host -Prompt 'Username'
+function Get-ConsoleCredential( [String] $username=$( read-host 'Username' ) ) {
+	while( !($username) ) {
+		$username = read-host 'Username'
 	}
 
 	$passwd = Read-Host -AsSecureString 'Password'
-	$cred = New-Object Management.Automation.PSCredential $Username, $passwd
-    return $cred
+	new-object Management.Automation.PSCredential $username, $passwd
 }
 
 New-Alias -Name 'su' -Value 'Switch-User'
